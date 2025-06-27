4aab78fd83f675ad445c3bfeefe3f30c62306594
Gurkaran Singh <gurkaransingh017gh@gmail.com>
2025-06-27 09:29:25 +0530

    Coffee & Bean List

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,15 @@
-// module.exports = {
-//   presets: ['module:@react-native/babel-preset'],
-//   plugins: ['@babel/plugin-transform-react-jsx'], // 👈 Add this line
-// };
 module.exports = {
-  presets: [['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }]],
-  plugins: [
-    [
-      '@babel/plugin-transform-react-jsx',
-      {
-        runtime: 'automatic',
-      },
-    ],
-  ]
-}
+  presets: ['module:@react-native/babel-preset'],
+  // plugins: ['@babel/plugin-transform-react-jsx'], // 👈 Add this line
+};
+// module.exports = {
+//   presets: [['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }]],
+//   plugins: [
+//     [
+//       '@babel/plugin-transform-react-jsx',
+//       {
+//         runtime: 'automatic',
+//       },
+//     ],
+//   ]
+// }
